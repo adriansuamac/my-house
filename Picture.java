3367947d49955489fc58f693cd5aa51d49bfd2ee
adrian <adrian@programa.com>
2024-09-30 13:30:32 +0200

    Modificando el color y el tamaño de Picture y cambiandolo de lado

--- Picture.java
+++ Picture.java
@@ -15,6 +15,7 @@ public class Picture
     private Square window;
     private Triangle roof;
     private Circle sun;
+    private Circle moon;
 
     /**
      * Constructor for objects of class Picture
@@ -54,6 +55,11 @@ public class Picture
         sun.moveVertical(-40);
         sun.changeSize(80);
         sun.makeVisible();
+        moon = new Circle();
+        moon.makeVisible();
+        moon.changeColor("magneta");
+        moon.moveHorizontal(-200);
+        moon.changeSize(40);
     }
 
     /**
